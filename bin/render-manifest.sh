c870f2b168a1dff26388ef42b14328afb23efd3a
eliranm <eliranm@liveperson.com>
2017-04-03 14:37:28 +0300

    experimenting with bump-version script

--- bin/render-manifest.sh
+++ bin/render-manifest.sh
@@ -3,40 +3,37 @@
 
 main() {
 	local source_dir="$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )"
-	local root_dir=${source_dir}/..
-	
-	
 	ensure_output_file
-
 	local commit_message="$(get_commit_message)"
-	echo "commit message: $commit_message"
-
 	render_manifest_template
-	
 	publish_manifest "$commit_message"
-
 }
 
 ensure_output_file() {
-	OUTPUT_FILE="${OUTPUT_FILE:-${source_dir}/../MANIFEST}"
+	local git_root="$(git rev-parse --show-toplevel)"
+	OUTPUT_FILE="${OUTPUT_FILE:-${git_root}MANIFEST}"
 }
 
 get_commit_message() {
-	local commit_message
+	local message
 	if [[ ! -f $OUTPUT_FILE ]]; then
-		commit_message="add manifest file"
+		message="add manifest file"
 	else
-		commit_message="update manifest"
+		message="update manifest"
 	fi
-	printf "%s" "$commit_message"
+	printf "%s" "$message"
 }
 
 render_manifest_template() {
-	local template_file=${source_dir}/manifest.tmpl
+	local template_file=${source_dir}/manifest.template
 	# declare manifest properties
 	local version="$(cat ${source_dir}/../VERSION)"
 	# inject values into placeholders and write to output file
-	sed -e 's,@@VERSION@@,'"$version"',g' ${template_file} > ${OUTPUT_FILE}
+	render_template ${template_file} > ${OUTPUT_FILE}
+}
+
+render_template() {
+	eval "echo \"$(cat $1)\""
 }
 
 publish_manifest() {
@@ -44,12 +41,6 @@ publish_manifest() {
 	git add ${OUTPUT_FILE}
 	git commit --only -m "$commit_message" -- ${OUTPUT_FILE}
 	git push origin
-#	if [[ $(has_changed "$OUTPUT_FILE") ]]; then
-#	fi
-}
-
-has_changed() {
-	git diff --name-only | grep "$(basename "$1")"
 }
 
 
