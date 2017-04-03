989c418008c1f67aa9f64de8a35b1aeac84e3f48
eliranm <eliranm@liveperson.com>
2017-04-03 14:54:53 +0300

    experimenting with bump-version script

--- bin/render-manifest.sh
+++ bin/render-manifest.sh
@@ -3,33 +3,34 @@
 
 main() {
 	local source_dir="$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )"
+	
+	ensure_manifest_file
 	ensure_output_file
+	
 	local commit_message="$(get_commit_message)"
+	
 	render_manifest_template
 	publish_manifest "$commit_message"
 }
 
+ensure_manifest_file() {
+	TEMPLATE_FILE="${TEMPLATE_FILE:-${source_dir}/manifest.template}"
+}
+
 ensure_output_file() {
 	local git_root="$(git rev-parse --show-toplevel)"
-	OUTPUT_FILE="${OUTPUT_FILE:-${git_root}MANIFEST}"
+	OUTPUT_FILE="${OUTPUT_FILE:-${git_root}/MANIFEST}"
 }
 
 get_commit_message() {
-	local message
-	if [[ ! -f $OUTPUT_FILE ]]; then
-		message="add manifest file"
-	else
-		message="update manifest"
-	fi
-	printf "%s" "$message"
+	[[ -f $OUTPUT_FILE ]] && { echo "update manifest" } || { echo "add manifest file" }
 }
 
 render_manifest_template() {
-	local template_file=${source_dir}/manifest.template
 	# declare manifest properties
 	local version="$(cat ${source_dir}/../VERSION)"
 	# inject values into placeholders and write to output file
-	render_template ${template_file} > ${OUTPUT_FILE}
+	render_template ${TEMPLATE_FILE} > ${OUTPUT_FILE}
 }
 
 render_template() {
